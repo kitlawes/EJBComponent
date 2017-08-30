0cd32d92e07a75fdcf8e285cfdf9d73ed0a15e81
Kit Lawes <kitlawes@hotmail.com>
2017-08-30 11:43:13 +0100

    Add LibrarySessionBeanRemote.java and LibrarySessionBean.java as an example of an EJB

---
+++ src/java/com/tutorialspoint/stateless/LibrarySessionBean.java
@@ -0,0 +1,23 @@
+package com.tutorialspoint.stateless;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+
+@Stateless
+public class LibrarySessionBean implements LibrarySessionBeanRemote {
+   
+   List<String> bookShelf;    
+   
+   public LibrarySessionBean(){
+      bookShelf = new ArrayList<String>();
+   }
+   
+   public void addBook(String bookName) {
+      bookShelf.add(bookName);
+   }    
+   
+   public List<String> getBooks() {
+      return bookShelf;
+   }
+}
