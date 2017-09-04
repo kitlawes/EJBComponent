eb62e260a5aa574aff53c0edb14df8c7f4a9b8bb
Kit Lawes <kitlawes@hotmail.com>
2017-09-04 09:57:35 +0100

    Update LibraryPersistentBean.java, LibraryPersistentBeanRemote.java, Book.java, jboss-ds.xml, and persistence.xml to demonstrate persistence in EJB

---
+++ src/java/com/tutorialspoint/entity/Book.java
@@ -0,0 +1,49 @@
+package com.tutorialspoint.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "books")
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 50)
+    @Column(name = "name")
+    private String name;
+
+    public Book() {
+    }
+
+    public Book(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
